7af6ed779169578029216107757ad9189563fdf2
Paul Moscoso Garay <pauluni10@hotmail.com>
2023-08-30 12:08:14 +0200

    Aggiunta funzionalità SignalR

---
+++ ServerTrace/ServerTrace/ServerTrace/Middleware/ApplicationBuilderExtension.cs
@@ -0,0 +1,16 @@
+﻿using ServerTrace.SubscribeTableDependencies;
+
+namespace ServerTrace.Middleware
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static void UseSqlTableDependency<T>(this IApplicationBuilder applicationBuilder, string connectionString)
+    where T : ISubscribeTableDependency
+        {
+            var serviceProvider = applicationBuilder.ApplicationServices;
+            var service = serviceProvider.GetService<T>();
+            service.SubscribeTableDependency(connectionString);
+        }
+
+    }
+}
---
+++ ServerTrace/ServerTrace/ServerTrace/SubscribeTableDependencies/SubscribeDevTracerDbTabledDependency.cs
@@ -0,0 +1,40 @@
+﻿using ServerTrace.Hubs;
+using ServerTrace.Models;
+using TableDependency.SqlClient;
+
+namespace ServerTrace.SubscribeTableDependencies
+{
+    public class SubscribeDevTracerDbTabledDependency : ISubscribeTableDependency
+    {
+
+        SqlTableDependency<DEV_TracerDB> tableDependency;
+        DashboardHub dashboardHub;
+
+        public SubscribeDevTracerDbTabledDependency(DashboardHub dashboardHub)
+        {
+            this.dashboardHub = dashboardHub;
+        }
+
+        public void SubscribeTableDependency(string connectionString)
+        {
+            tableDependency = new SqlTableDependency<DEV_TracerDB>(connectionString);
+            tableDependency.OnChanged += TableDependency_OnChanged;
+            tableDependency.OnError += TableDependency_OnError;
+            tableDependency.Start();
+        }
+
+        private void TableDependency_OnChanged(object sender, TableDependency.SqlClient.Base.EventArgs.RecordChangedEventArgs<DEV_TracerDB> e)
+        {
+            if (e.ChangeType != TableDependency.SqlClient.Base.Enums.ChangeType.None)
+            {
+                dashboardHub.SendDevTracer();
+            }
+        }
+
+        private void TableDependency_OnError(object sender, TableDependency.SqlClient.Base.EventArgs.ErrorEventArgs e)
+        {
+            Console.WriteLine($"{nameof(DEV_TracerDB)} SqlTableDependency error: {e.Error.Message}");
+        }
+
+    }
+}
