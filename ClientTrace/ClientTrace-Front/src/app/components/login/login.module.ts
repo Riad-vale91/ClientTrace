b77a026f21639eff2aaaee20442028899880ffa1
Vale-riad <valentinariad91@gmail.com>
2023-09-06 14:54:57 +0200

    Aggiunge modifiche

---
+++ ClientTrace/ClientTrace-Front/src/app/components/login/login.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    HttpClientModule,
+  ],
+  exports:[
+    LoginComponent
+  ]
+})
+export class LoginModule { }
